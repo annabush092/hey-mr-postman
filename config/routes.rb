763364c2c65110edc2db3b611b69f2411e912dce
annabush092 <annabush092@gmail.com>
2017-11-10 09:45:42 -0500

    made login a post request

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :emails, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:index, :show, :create]
-      get '/login/:name', to: 'users#login'
+      post '/login/:name', to: 'users#login'
     end
   end
 
